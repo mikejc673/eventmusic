be027de13deb95177aa236871a297a98f7d40a33
mikejc673 <mikejc673@gmail.com>
2025-09-05 11:29:20 +0200

    update

---
+++ mon_site/Form/TaskType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Task;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('task', TextType::class, [
+                'label' => 'Tâche',
+            ])
+            ->add('dueDate', DateTimeType::class, [
+                'label' => 'Date limite',
+                'required' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
--- mon_site/src/Controller/TaskController.php
+++ mon_site/src/Controller/TaskController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class TaskController extends AbstractController
 {
     #[Route('/task/{id}', name: 'task_show')]
@@ -28,6 +29,40 @@ class TaskController extends AbstractController
         $tasks = $em->getRepository(Task::class)->findAll();
         return $this->render('task_list.html.twig', [
             'tasks' => $tasks,
+        ]);
+    }
+
+    #[Route('/task/new', name: 'task_new')]
+    public function new(EntityManagerInterface $em, \Symfony\Component\HttpFoundation\Request $request): Response
+    {
+        $task = new Task();
+        $form = $this->createForm(\App\Form\TaskType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($task);
+            $em->flush();
+            return $this->redirectToRoute('task_list');
+        }
+
+        return $this->render('task_form.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/task/{id}/edit', name: 'task_edit')]
+    public function edit(Task $task, EntityManagerInterface $em, \Symfony\Component\HttpFoundation\Request $request): Response
+    {
+        $form = $this->createForm(\App\Form\TaskType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            return $this->redirectToRoute('task_list');
+        }
+
+        return $this->render('task_form.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
