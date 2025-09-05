695a920a8bdc831c8ebb09ac25643c27ceb8498c
mikejc673 <mikejc673@gmail.com>
2025-09-05 13:55:07 +0200

    update controller

---
+++ mon_site/src/Controller/UserController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/user/new', name: 'user_new')]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($user);
+            $em->flush();
+            return $this->redirectToRoute('user_list');
+        }
+
+        return $this->render('user_form.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/user', name: 'user_list')]
+    public function list(EntityManagerInterface $em): Response
+    {
+        $users = $em->getRepository(User::class)->findAll();
+        return $this->render('user_list.html.twig', [
+            'users' => $users,
+        ]);
+    }
+}
